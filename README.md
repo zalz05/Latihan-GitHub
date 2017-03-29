d77e3b35a9a0b3185022d636f5cf8906189d3f6f
pryamm <pryammx@gmail.com>
2017-03-29 21:30:28 +0700

    Import Latihan-Github Test

---
+++ README.md
@@ -0,0 +1,8 @@
+#Latihan
+
+Latihan GitHub
+1. dilakukan untuk mempelajari github
+2. dapat digunakan oleh publik
+
+Sumber :
+-
---
+++ contoh.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>LATIHAN GIT & GITHUB</title>
+</head>
+<body>
+<?php
+
+echo ' ini latihan github ';
+
+?>
+
+
+
+</body>
+</html>
